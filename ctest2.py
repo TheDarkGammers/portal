950f94486383d619a49071a29479ab9715e7bc79
TheDarkGammers <98757458+TheDarkGammers@users.noreply.github.com>
2022-10-19 11:14:06 -0500

    Create ctest2.py

---
+++ ctest2.py
@@ -0,0 +1,29 @@
+import time
+import os
+import board
+import digitalio
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load("thunder.wav")
+pygame.mixer.music.play()
+
+
+s1 = digitalio.DigitalInOut(board.D17)
+s1.direction = digitalio.Direction.INPUT
+s1.pull = digitalio.Pull.DOWN
+
+isopen = s1.value
+
+
+while True:
+        #if (s1.value) == True:
+        #       print("closed")
+        #else:
+        #        print("open")
+        if (s1.value != isopen):
+                isopen = s1.value
+                if isopen == True:
+                        print("closed")
+                else:
+                        print("open")
+        time.sleep(0.05)
