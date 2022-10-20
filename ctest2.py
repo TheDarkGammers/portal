1021532f929ce98536a3dc8dde74ef624c9f27d4
TheDarkGammers <98757458+TheDarkGammers@users.noreply.github.com>
2022-10-20 10:49:50 -0500

    bruh

--- ctest2.py
+++ ctest2.py
@@ -4,7 +4,7 @@ import board
 import digitalio
 import pygame
 pygame.mixer.init()
-pygame.mixer.music.load("halotheme_clip.wav")
+pygame.mixer.music.load("thunder.wav")
 pygame.mixer.music.play()
 
 s1 = digitalio.DigitalInOut(board.D17)
