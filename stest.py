94d62bd760887d3deb5f894a68fd362c6c3188d0
TheDarkGammers <98757458+TheDarkGammers@users.noreply.github.com>
2022-10-20 10:47:12 -0500

    e

--- stest.py
+++ stest.py
@@ -4,5 +4,5 @@ import board
 import digitalio
 import pygame
 pygame.mixer.init()
-pygame.mixer.music.load("Halo Theme Song Original clip.wav")
+pygame.mixer.music.load("thunder.wav")
 pygame.mixer.music.play()
