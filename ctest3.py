397faa6d3d14112605f93cb13aee1ef46be0fb1d
TheDarkGammers <98757458+TheDarkGammers@users.noreply.github.com>
2022-11-30 15:26:51 -0600

    i'm dumb

--- ctest3.py
+++ ctest3.py
@@ -4,6 +4,7 @@ import subprocess
 import board
 import digitalio
 
+os.system(f"sudo python3 /home/pi/door/led_startup.py &")
 
 os.system('amixer cset numid=1 100%')
 os.system("cvlc --play-and-exit /home/pi/portal/portal_start.mp3")
@@ -26,8 +27,10 @@ while True:
                 isopen = s1.value
                 if isopen == True:
                         print("closed")
+                        os.system(f"sudo python3 /home/pi/door/led_close.py &")
                 else:
                         print("open")
+                        os.system(f"sudo python3 /home/pi/door/led_open.py &")
                         os.system("cvlc --play-and-exit /home/pi/portal/bbc_comedy-sou_07005034.mp3")
 
         time.sleep(0.05)
