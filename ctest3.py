3f6f2ecebb49df0c515ced8031856363855a1c56
TheDarkGammers <98757458+TheDarkGammers@users.noreply.github.com>
2022-12-12 15:15:28 -0600

    please

--- ctest3.py
+++ ctest3.py
@@ -39,7 +39,7 @@ while True:
                         print("open")
                         os.system(f"sudo python3 /home/pi/door/led_open.py &")
                         soundFile = f"/home/pi/portal/Sounds/{random.choices(filelist, weights = (30,10), k = 1)[0]}"
-                        cmd = "cvlc --play-and-exit {soundFile}"
+                        cmd = f"cvlc --play-and-exit {soundFile}"
                         print ("sound",cmd)
                         os.system(cmd)
                 
